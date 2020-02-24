e554ed8813b0eb5889cdf8c912e192dd72d554ac
Avdi Grimm <avdi@avdi.codes>
2020-02-24 15:17:16 -0600

    Use EDD-provided session-limiting code
    Replaces Jess' experimental version 😢

--- wp-content/plugins/avdi-codes/avdi-codes.php
+++ wp-content/plugins/avdi-codes/avdi-codes.php
@@ -12,29 +12,35 @@ function remove_admin_bar() {
   }
 } 
 
-global $wp;
-global $wp_query;
-global $pagename;
+/*
+ * Only start an EDD session if necessary.
+ * 
+ * This avoids busting the cache for every page on the site.
+ * 
+ * Originally provided by Daniel Goldak at EDD support.
+ * Ref: https://app.frontapp.com/open/msg_a23om0z
+ *
+ * @param  bool $start_session
+ * @return bool
+ */
+function eddwp_maybe_start_session( $start_session ) {
+	if ( '/' == $_SERVER['REQUEST_URI'] ) {
+		$start_session = false;
+	}
+	if( false !== strpos( $_SERVER['REQUEST_URI'], '/downloads' ) && '/downloads/' === trailingslashit( $_SERVER['REQUEST_URI'] ) ) {
+		$start_session = false;
+	}
+	if( empty( $_REQUEST['edd_action'] ) && false === strpos( $_SERVER['REQUEST_URI'], '/downloads' ) ) {
+		$start_session = false;
+	}
 
-function say_no($before) {
-  // now can I figure out which page this is? I don't know!!
-  // it might be like $wp->request or like $wp_query->post or any number of things
-  // but I don't know how to even explore that. Or to log, or to get any output from here to anywhere.
-   $thispage=$_SERVER["REQUEST_URI"]; //print_r($wp_query);// add_query_arg(array(),$wp->request); //$wp_query->post->post_title;
-  
-  if (preg_match('/^\/[(blog)(contact)]/', $thispage)) {
-    function add_jess_header($whatevs) {
-      $thispage=$_SERVER["REQUEST_URI"];
-      header('X-Jess: yeah [' . $thispage . '] looks static to me. Not starting a session ');
-    }
-    add_action('send_headers', 'add_jess_header');
-    return false;
-  }   
-
-  return $before;
-
+	// Finally, if there is a discount in the GET parameters, we should always start a session, so it applies correctly.
+	if ( ! empty( $_GET['discount'] ) ) {
+		$start_session = true;
+	}
+	return $start_session;
 }
-add_filter('edd_start_session', 'say_no');
+add_filter( 'edd_start_session', 'eddwp_maybe_start_session', 10, 1 );
 
 /* Stop Adding Functions Below this Line */
 ?>
